d3e06fbd166f73f24eee3fec5868e4e112a2077d
saferosatom <saferosatom@gmail.com>
2025-11-27 08:46:30 +0300

    20251127084630

--- push.sh
+++ push.sh
@@ -1,10 +1,9 @@
-if [ -z %1 ]; then
+#if [ -z %1 ]; then
     comm=$(date +%Y%m%d%H%M%S)
-else
-    comm=%1
-fi
-#$comm="\""+$comm+"\"
-#echo {$comm}
+#else
+#    comm=$1
+#fi
+
 git add .
 git commit -m "$comm"
 git push -u origin main
